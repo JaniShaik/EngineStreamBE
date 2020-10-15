7d95b6ab764f38def0582974d63c9d8ddeccf996
USER <USER@ITCRATS-LP-85>
2020-10-15 15:21:02 +0530

    testing...123

--- src/main/java/com/ste/inventorymanagement/InventorymanagementApplication.java
+++ src/main/java/com/ste/inventorymanagement/InventorymanagementApplication.java
@@ -8,6 +8,7 @@ public class InventorymanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventorymanagementApplication.class, args);
+		//checking
 	}
 
 }
