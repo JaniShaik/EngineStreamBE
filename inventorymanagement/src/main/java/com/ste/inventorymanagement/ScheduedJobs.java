786e89460a171b6782599628d3bb41d0cbbb12b3
leekendra <vleekendra1997@gmail.com>
2020-11-02 19:52:50 +0530

    parsing all sheets of excel and cron job is set to run automatically

--- inventorymanagement/src/main/java/com/ste/inventorymanagement/ScheduedJobs.java
+++ inventorymanagement/src/main/java/com/ste/inventorymanagement/ScheduedJobs.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.ste.inventorymanagement.controller.InsertDataFromExcelToDbController;
 import com.ste.inventorymanagement.repository.BatchRepository;
 import com.ste.inventorymanagement.repository.MaterialRepository;
 
@@ -15,6 +16,9 @@ public class ScheduedJobs {
 
 	@Autowired
 	BatchRepository batchRepo;
+	
+	@Autowired
+	InsertDataFromExcelToDbController insertDataFromExcelToDbController;
 	//@Scheduled(cron = "0 * 9 * * ?") 
 	// @Scheduled(fixedRate = 1000)
 	// @Scheduled(fixedDelay = 1000, initialDelay = 3000)
@@ -25,6 +29,15 @@ public class ScheduedJobs {
 	      String strDate = sdf.format(now);
 	      System.out.println("Java cron job expression:: " + strDate);
 	      batchRepo.updateBatchSurplusFlagJob();
+	   }
+	
+	@Scheduled(cron = "0 35 19 * * ?")
+	   public void cronJobInsertDataFromExcelToDbDetailsAndValues() throws Exception {
+	      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+	      Date now = new Date();
+	      String strDate = sdf.format(now);
+	      System.out.println("cronJob InsertDataFromExcelToDb Details And Values:: " + strDate);
+	      insertDataFromExcelToDbController.insertDataFromExcelToDb();
 	   }
 	
 }
--- inventorymanagement/src/main/java/com/ste/inventorymanagement/services/ReadExcelSheetService.java
+++ inventorymanagement/src/main/java/com/ste/inventorymanagement/services/ReadExcelSheetService.java
@@ -39,7 +39,9 @@ public class ReadExcelSheetService {
 	 * }
 	 */
 	public  void ReadExcel() throws Exception {
-		String fileLocation = "C:\\Users\\USER\\Downloads\\sample_excel_sheet\\Comp_Repair_CFM56-5B-2020_Catalog AUGUST 2020.xls";
+		//String fileLocation = "C:\\Users\\USER\\Downloads\\sample_excel_sheet\\Comp_Repair_CFM56-5B-2020_Catalog AUGUST 2020.xls";
+		String fileLocation = ".\\src\\main\\resources\\excel\\Comp_Repair_CFM56-5B-2020_Catalog AUGUST 2020.xls";
+
 		boolean insertIntoDetails = false;
 
 		//fields of details
@@ -66,136 +68,162 @@ public class ReadExcelSheetService {
 		org.apache.poi.ss.usermodel.Workbook workbook = null;
 		workbook = WorkbookFactory.create(fis);
 
-		org.apache.poi.ss.usermodel.Sheet sheet = workbook.getSheetAt(0);
+		org.apache.poi.ss.usermodel.Sheet sheet = workbook.getSheetAt(4);
 
-		sheet = workbook.getSheetAt(0);
-		
-		//getting name of the sheet i,e CFM56-5B
-		name = sheet.getRow(0).getCell(2).getStringCellValue().split(" ")[0];
-		name = name.split("\n")[0];
-		
-		for (int rn=3; rn<=sheet.getLastRowNum(); rn++) {
-			Row row = sheet.getRow(rn);
-			/*
-			 * System.out.println("no of cells "+row.getPhysicalNumberOfCells());
-			 * System.out.println("index "+rn);
-			 */
-
-			int detailsSno = 1;
-			int valuesSno = 1;
+		System.out.println("no of sheets"+workbook.getNumberOfSheets());
 
 
-			for (int cn=0; cn<row.getLastCellNum(); cn++) {
-				Cell cell = row.getCell(cn);
+		for(int i = 0; i < workbook.getNumberOfSheets(); i++) {
+			sheet = workbook.getSheetAt(i);
 
-				//details
-				if(cn == 0 && cell != null && cell.getStringCellValue().length() > 1) {
-					insertIntoDetails = true;
+			//getting name of the sheet i,e CFM56-5B
+			name = sheet.getRow(0).getCell(2).getStringCellValue().split(" ")[0].split("\n")[0];
 
-				}
-				//values
-				if(cn == 0 && cell == null) {
-					insertIntoDetails = false;
-				}
+			for (int rn=3; rn<=sheet.getLastRowNum(); rn++) {
+				Row row = sheet.getRow(rn);
+				/*
+				 * System.out.println("no of cells "+row.getPhysicalNumberOfCells());
+				 * System.out.println("index "+rn);
+				 */
+
+				int detailsSno = 1;
+				int valuesSno = 1;
+
+
+				for (int cn=0; cn<row.getLastCellNum(); cn++) {
+					Cell cell = row.getCell(cn);
+
+					//details
+					if(cn == 0 && cell != null && cell.getStringCellValue().length() > 1) {
+						insertIntoDetails = true;
+
+					}
+					//values
+					if(cn == 0 && cell == null) {
+						insertIntoDetails = false;
+					}
+					if(insertIntoDetails) {
+						//insert into details table
+						switch (detailsSno++) {
+						case 1:
+							componentCode = cell != null ? cell.getStringCellValue() : null;
+							break;
+						case 2: 
+							componentParts = cell != null ? cell.getStringCellValue() : null;
+							break;
+						case 3: 
+							repairSites = cell != null ? cell.getStringCellValue() : null;
+							break;
+						case 4: 
+							ataRepairNumber = cell != null ? cell.getStringCellValue() : null;
+							break;
+						default:
+							String data = cell != null ? cell.getStringCellValue() : null;
+							//System.out.println("hey..we got some unexpected data "+detailsSno+" with data "+ data);
+						}
+
+					}
+					if(!insertIntoDetails) {
+						//insert into values table
+						switch (valuesSno++) {
+						case 1: break;
+						case 2: componentPartsValue = cell != null ? cell.getStringCellValue() : null;
+						break;
+						case 3:
+							repairSitesValue = cell != null ? cell.getStringCellValue() : null;
+							break;
+						case 4:
+							ataRepairNumberValue = cell != null ? cell.getStringCellValue() : null;
+							break;
+						case 5:
+							repairDescription = cell != null ? cell.getStringCellValue() : null;
+							break;
+						case 6:
+							tatDays = cell != null ? cell.getStringCellValue() : null;
+							tatDays = tatDays != null ? tatDays.replace("+", "") : null; 
+							break;
+						case 7:
+							price = cell != null ? cell.getStringCellValue() : null;
+							price = price != null ? price.replace("+", "") : null; 
+							break;
+						default:
+							//System.out.println("hey..we got some unexpected value "+valuesSno+ "with value "+cell.getStringCellValue());
+						}
+
+
+					}
+
+				} //end of for
+
+
 				if(insertIntoDetails) {
-					//insert into details table
-					switch (detailsSno++) {
-					case 1:
-						componentCode = cell != null ? cell.getStringCellValue() : null;
-						break;
-					case 2: 
-						componentParts = cell != null ? cell.getStringCellValue() : null;
-						break;
-					case 3: 
-						repairSites = cell != null ? cell.getStringCellValue() : null;
-						break;
-					case 4: 
-						ataRepairNumber = cell != null ? cell.getStringCellValue() : null;
-						break;
-					default:
-						String data = cell != null ? cell.getStringCellValue() : null;
-						//System.out.println("hey..we got some unexpected data "+detailsSno+" with data "+ data);
+					//save into details
+					catalogueComponentDetails = new CatalogueComponentDetails();
+					catalogueComponentDetails.setName(name);
+					catalogueComponentDetails.setComponentCode(componentCode);
+					catalogueComponentDetails.setComponentParts(componentParts);
+					catalogueComponentDetails.setRepairSites(repairSites);
+					catalogueComponentDetails.setAtaRepairNumber(ataRepairNumber);
+
+					detailsRepo.save(catalogueComponentDetails);
+
+					componentCode = null;
+					componentParts = null;
+					repairSites = null;
+					ataRepairNumber = null;
+
+				} else {
+					//save into values
+					if(repairDescription != null && repairDescription.contains("*Note")) {
+						componentPartsValue = null;
+						ataRepairNumberValue = null;
+						repairDescription = null;
+						tatDays = null;
+						price = null;
+						repairSitesValue = null;
+
+						continue;
 					}
 
-				}
-				if(!insertIntoDetails) {
-					//insert into values table
-					switch (valuesSno++) {
-					case 1: break;
-					case 2: componentPartsValue = cell != null ? cell.getStringCellValue() : null;
-					break;
-					case 3:
-						repairSitesValue = cell != null ? cell.getStringCellValue() : null;
-						break;
-					case 4:
-						ataRepairNumberValue = cell != null ? cell.getStringCellValue() : null;
-						break;
-					case 5:
-						repairDescription = cell != null ? cell.getStringCellValue() : null;
-						break;
-					case 6:
-						tatDays = cell != null ? cell.getStringCellValue() : null;
-						tatDays = tatDays != null ? tatDays.replace("+", "") : null; 
-						break;
-					case 7:
-						price = cell != null ? cell.getStringCellValue() : null;
-						price = price != null ? price.replace("+", "") : null; 
-						break;
-					default:
-						//System.out.println("hey..we got some unexpected value "+valuesSno+ "with value "+cell.getStringCellValue());
+					catalogueComponentValues = new CatalogueComponentValues();
+					catalogueComponentValues.setCatalogueComponentDetails(
+							catalogueComponentDetails);
+					catalogueComponentValues.setComponentPartsValue(componentPartsValue);
+					catalogueComponentValues.setAtaRepairNumberValue(ataRepairNumberValue);
+					catalogueComponentValues.setPrice(price);
+					catalogueComponentValues.setRepairDescription(repairDescription);
+					catalogueComponentValues.setRepairSitesValue(repairSitesValue);
+					catalogueComponentValues.setTatDays(tatDays);
+
+					String isEmpty = componentPartsValue + ataRepairNumberValue + price + repairDescription + repairSitesValue + tatDays;
+					isEmpty = isEmpty.replace(" ", "");
+					isEmpty = isEmpty.replace("null", "");
+					if(isEmpty.equals("")) {
+						componentPartsValue = null;
+						ataRepairNumberValue = null;
+						repairDescription = null;
+						tatDays = null;
+						price = null;
+						repairSitesValue = null;
+
+						continue;
 					}
 
+					valuesRepo.save(catalogueComponentValues);
 
+					componentPartsValue = null;
+					ataRepairNumberValue = null;
+					repairDescription = null;
+					tatDays = null;
+					price = null;
+					repairSitesValue = null;
 				}
-				
-			} //end of for
-
-			if(insertIntoDetails) {
-				//save into details
-				catalogueComponentDetails = new CatalogueComponentDetails();
-				catalogueComponentDetails.setName(name);
-				catalogueComponentDetails.setComponentCode(componentCode);
-				catalogueComponentDetails.setComponentParts(componentParts);
-				catalogueComponentDetails.setRepairSites(repairSites);
-				catalogueComponentDetails.setAtaRepairNumber(ataRepairNumber);
-
-				detailsRepo.save(catalogueComponentDetails);
-
-				componentCode = null;
-				componentParts = null;
-				repairSites = null;
-				ataRepairNumber = null;
-
-			} else {
-				//save into values
-				if(repairDescription != null && repairDescription.contains("*Note"))
-					continue;
-				
-				catalogueComponentValues = new CatalogueComponentValues();
-				catalogueComponentValues.setCatalogueComponentDetails(
-						catalogueComponentDetails);
-				catalogueComponentValues.setComponentPartsValue(componentPartsValue);
-				catalogueComponentValues.setAtaRepairNumberValue(ataRepairNumberValue);
-				catalogueComponentValues.setPrice(price);
-				catalogueComponentValues.setRepairDescription(repairDescription);
-				catalogueComponentValues.setRepairSitesValue(repairSitesValue);
-				catalogueComponentValues.setTatDays(tatDays);
-				
-				String isEmpty = componentPartsValue + ataRepairNumberValue + price + repairDescription + repairSitesValue + tatDays;
-				if(isEmpty.replace(" ", "").equals(""))
-					continue;
-
-				valuesRepo.save(catalogueComponentValues);
-
-				componentPartsValue = null;
-				ataRepairNumberValue = null;
-				repairDescription = null;
-				tatDays = null;
-				price = null;
-				repairSitesValue = null;
+				//System.out.println();
 			}
-			//System.out.println();
+
 		}
+
+
 	}
 }
 
